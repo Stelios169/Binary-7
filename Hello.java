67b35703712790de27bb1dbc55499b3b9244b47d
mariakontopoulou <t8230061@aueb.gr>
2024-11-10 22:20:55 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-    public static void main[String args] {
-        return "hello there";
+    public static void main(String[] args) {
+        System.out.println ("hello there");
     }
 }
