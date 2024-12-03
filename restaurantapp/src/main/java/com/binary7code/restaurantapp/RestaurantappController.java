88e0e0563a722af22b0d99214cf9934ef82d5778
Angelos Tsekos <t823012@aueb.gr>
2024-12-03 14:17:53 +0000

    Βασιλης Τσεκος

--- restaurantapp/src/main/java/com/binary7code/restaurantapp/RestaurantappController.java
+++ restaurantapp/src/main/java/com/binary7code/restaurantapp/RestaurantappController.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Controller;
 public class RestaurantappController {
     @GetMapping("/")
     public String index() {
-        return "redirect:/index.html";
+        return "index";
     }
 }
