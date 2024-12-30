d7e1c923dea2121eb72d324b7fc740e7cfe2e696
DESPOINA LAMPROU <t8230072@aueb.gr>
2024-12-30 15:01:21 +0000

    adding models and dependencies

---
+++ demo/src/main/java/com/example/demo/models/DOid.java
@@ -0,0 +1,16 @@
+package com.example.demo.models;
+
+import lombok.Data;
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@Data 
+
+public class DOid implements Serializable {
+    private int order_id;
+    private int dish_id;    
+}
