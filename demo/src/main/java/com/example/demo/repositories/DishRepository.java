fc1fcdd2e813139774df38015c143702bd9ece1f
Ioanna Malaki <t8230085@aueb.gr>
2025-01-03 12:47:26 +0000

    made some changes

--- demo/src/main/java/com/example/demo/repositories/DishRepository.java
+++ demo/src/main/java/com/example/demo/repositories/DishRepository.java
@@ -1,24 +1,15 @@
 package com.example.demo.repositories;
 
-import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import com.example.demo.models.Dish;
-import com.example.demo.models.DishProfit;
+
 import java.util.Optional;
  
 public interface DishRepository extends JpaRepository<Dish, Integer> {
-    @Query("""
-        SELECT new com.example.dto.DishProfit(d.id, d.name, d.restaurant.id, d.price, 
-            (SUM(i.cost * di.quantity)), (d.price - SUM(i.cost * di.quantity)), dc.name)
-        FROM Dish d
-        JOIN d.ingredients di
-        JOIN di.ingredient i
-        JOIN d.category dc
-        GROUP BY d.id, d.name, d.restaurant.id, d.price, dc.name
-    """)
-    List<DishProfit> calculateAndSortDishProfit(); 
+    
         @Query("SELECT r.restaurant_email FROM Restaurant r " +
        "JOIN r.dishes d " +
        "JOIN d.ingredients di " +
--- demo/src/main/java/com/example/demo/repositories/IngredientRepository.java
+++ demo/src/main/java/com/example/demo/repositories/IngredientRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repositories;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.models.Ingredient;
  
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
 
-    List<Ingredient> findByDishId(int dishId);
+    
     
 }
