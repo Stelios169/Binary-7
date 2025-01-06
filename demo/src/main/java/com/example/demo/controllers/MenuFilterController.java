8a7f0756078620089981b208efbba45acb66a584
Ioanna Malaki <t8230085@aueb.gr>
2025-01-06 16:03:26 +0000

    added the Menufilter feature

---
+++ demo/src/main/java/com/example/demo/controllers/MenuFilterController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Dish;
+import com.example.demo.services.MenuFilterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/menu")
+public class MenuFilterController {
+
+    private final MenuFilterService menuFilterService;
+
+    @Autowired
+    public MenuFilterController(MenuFilterService menuFilterService) {
+        this.menuFilterService = menuFilterService;
+    }
+
+    @GetMapping("/view")
+    public Map<String, List<Dish>> viewMenu() {
+        return menuFilterService.viewMenu();
+    }
+
+    @PostMapping("/filter")
+    public List<Dish> filterMenu(@RequestParam double budget,
+                                 @RequestParam String[] categories,
+                                 @RequestParam String[] allergies) {
+        return menuFilterService.filterMenu(budget, categories, allergies);
+    }
+}
