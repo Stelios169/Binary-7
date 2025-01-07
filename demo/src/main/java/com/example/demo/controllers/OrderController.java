316edec3da47f480ca97687cfb7b2a1c10feeeef
poullakkos <t8230126@aueb.gr>
2025-01-07 15:57:53 +0000

    Add-dish table and other changes

--- demo/src/main/java/com/example/demo/controllers/OrderController.java
+++ demo/src/main/java/com/example/demo/controllers/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/menu")
 public class OrderController {
 
     private final OrderService orderService;
--- demo/src/main/java/com/example/demo/statistics/dto/FavoriteDishDTO.java
+++ demo/src/main/java/com/example/demo/statistics/dto/FavoriteDishDTO.java
@@ -15,5 +15,3 @@ public class FavoriteDishDTO {
         this.dish_name = dish_name;
     }
 }
-
-    
