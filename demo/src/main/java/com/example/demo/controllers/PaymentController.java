e69ae31efca9c44005e624e61b4dd038d70ab356
Stefania Georgiou <t8230021@aueb.gr>
2025-01-13 17:25:18 +0000

    Changes in PaymentController

--- demo/src/main/java/com/example/demo/controllers/PaymentController.java
+++ demo/src/main/java/com/example/demo/controllers/PaymentController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.services.PaymentService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,13 +15,15 @@ public class PaymentController {
     }
 
     @PostMapping
-    public String processPayment(@RequestParam int tableId, @RequestParam int restaurantId) {
+    public String processPayment(@RequestParam int tableId, @RequestParam int restaurantId, Model model) {
         float totalAmount = paymentService.processPayment(tableId, restaurantId);
 
         if (totalAmount > 0) {
-            return String.format("Payment successful. Total amount: %.2f€", totalAmount);
+            model.addAttribute("message", String.format("Payment successful. Total amount: %.2f€", totalAmount));
         } else {
-            return "No active orders for this table.";
+            model.addAttribute("message", "No active orders for this table.");
         }
-    } 
+        
+        return "paymentResult";
+    }
 }
