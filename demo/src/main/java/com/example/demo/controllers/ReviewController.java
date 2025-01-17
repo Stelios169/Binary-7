50b8e51dcad34d1d2486ddd76996b626b1d391e6
poullakkos <t8230126@aueb.gr>
2025-01-17 12:56:44 +0000

    review page

--- demo/src/main/java/com/example/demo/controllers/ReviewController.java
+++ demo/src/main/java/com/example/demo/controllers/ReviewController.java
@@ -1,8 +1,12 @@
 
 package com.example.demo.controllers;
 
+import com.example.demo.models.Restaurant;
 import com.example.demo.models.Review;
+import com.example.demo.repositories.RestaurantRepository;
 import com.example.demo.services.ReviewService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,19 +20,27 @@ public class ReviewController {
 
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
+
     }
 
-    @GetMapping("/{restaurant_id}")
-    public String getReviewsByRestaurant(@RequestParam int restaurant_id, Model model) {
+    @GetMapping("/reviewList")
+    public String getReviewsByRestaurant(@RequestParam(defaultValue = "1") int restaurant_id, Model model) {
         List<Review> reviews = reviewService.getReviewsByRestaurant(restaurant_id);
+        model.addAttribute("restaurant_id", restaurant_id);
         model.addAttribute("reviews", reviews);
+        model.addAttribute("review", new Review());
         return "reviewList";
     }
 
     @PostMapping("/reviewList")
-    public String addReview(@ModelAttribute Review review, @RequestParam int restaurant_id, Model model) {
+    public String addReview(@ModelAttribute Review review, @RequestParam int restaurant_id,
+            Model model) {
+
         Review savedReview = reviewService.addReview(review, restaurant_id);
+        List<Review> reviews = reviewService.getReviewsByRestaurant(restaurant_id);
+        model.addAttribute("restaurant_id", restaurant_id);
         model.addAttribute("review", savedReview);
+        model.addAttribute("reviews", reviews);
         return "reviewList";
     }
 }
